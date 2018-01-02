a67130f7994a18d5e27f5b3a1ae99dfbdc3f5f19
DavidT3 <dt237@sussex.ac.uk>
2018-01-02 15:37:01 +0000

    Enables error extraction and saving to session log

--- ThomasTemplates.py
+++ ThomasTemplates.py
@@ -113,8 +113,6 @@ fit 100
 breakLinks {{{{Abundanc ""}}}}
 fit
 puts $runit
-puts [massOfR {r500} rbounds]
-fit
 log
 puts [MassConf radius {r500} rbounds 0.01]
 log none
@@ -123,7 +121,7 @@ set result {{#Better fit found.  Rerun fit now.}}
 set term {{#Better fit found.  Rerun fit now.}}
  while {{$result==$term}} {{
     set fp [open xspec.log]
-    set data [split [read $fp] \n]
+    set data [split [read $fp] \\n]
     set result [lsearch -exact -inline $data $term]
     puts $result
     close $fp
@@ -133,11 +131,13 @@ set term {{#Better fit found.  Rerun fit now.}}
       log
       puts [MassConf radius {r500} rbounds 0.01]
       log none
-      puts success
     }}
  }}
+puts choochoo
 exit
 """  # template clmass script, requires: data_groups, model_path, model, nH, kT, z, r500
+
+# puts [massOfR {r500} rbounds] just gets the mass without conf limits
 
 d = {#'arfgen_job': arfgen_job,
      'pn_file': pn_file,
